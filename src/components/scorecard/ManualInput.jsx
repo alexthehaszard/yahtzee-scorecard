eaf4a0e2f3bcd396fd61d0614e5daeace5f58000
Alex Haszard <aggis.haszard@gmail.com>
2025-01-18 12:12:45 +1300

    add invalid popup when score > 30

--- src/components/scorecard/ManualInput.jsx
+++ src/components/scorecard/ManualInput.jsx
@@ -6,9 +6,13 @@ export const ManualInput = (props) => {
       <input
         type="number"
         onChange={(event) => props.oninput(event.target.value)}
+        style={{ borderColor: props.value > 30 ? "red" : "white" }}
         placeholder="0"
         inputMode="numeric"
       />
+      <span style={{ display: props.value > 30 ? "flex" : "none" }}>
+        Invalid
+      </span>
     </div>
   );
 };
